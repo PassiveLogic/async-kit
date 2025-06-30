030df9643191ae08a686d54f5ddde8c0952acb79
Scott Marchant <scottm@passivelogic.com>
2025-06-30 15:36:00 -0600

    feat: Add shims for GCD for wasm-specific builds. Copied and adapted from dispatch-async. See https://github.com/PassiveLogic/dispatch-async.

---
+++ Sources/AsyncKit/DispatchAsyncWasm/DispatchGroup.swift
@@ -0,0 +1,135 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift.org project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift.org project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+#if os(WASI) && !canImport(Dispatch)
+
+// MARK: - Interface for Non-Async Usage -
+
+/// `DispatchGroup` is a drop-in replacement for the `DispatchGroup` implemented
+/// in Grand Central Dispatch. However, this class uses Swift Concurrency, instead of low-level threading API's.
+///
+/// The primary goal of this implementation is to enable WASM support for Dispatch.
+///
+/// Refer to documentation for the original [DispatchGroup](https://developer.apple.com/documentation/dispatch/dispatchgroup)
+/// for more details,
+@available(macOS 10.15, *)
+class DispatchGroup: @unchecked Sendable {
+    /// Used to ensure FIFO access to the enter and leave calls
+    @globalActor
+    private actor DispatchGroupEntryActor: GlobalActor {
+        static let shared = DispatchGroupEntryActor()
+    }
+
+    private let group = AsyncGroup()
+
+    func enter() {
+        Task { @DispatchGroupEntryActor [] in
+            // ^--- Ensures serial FIFO entrance/exit into the group
+            await group.enter()
+        }
+    }
+
+    func leave() {
+        Task { @DispatchGroupEntryActor [] in
+            // ^--- Ensures serial FIFO entrance/exit into the group
+            await group.leave()
+        }
+    }
+
+    func notify(queue: DispatchQueue, execute work: @escaping @Sendable @convention(block) () -> Void) {
+        Task { @DispatchGroupEntryActor [] in
+            // ^--- Ensures serial FIFO entrance/exit into the group
+            await group.notify {
+                await withCheckedContinuation { continuation in
+                    queue.async {
+                        work()
+                        continuation.resume()
+                    }
+                }
+            }
+        }
+    }
+
+    func wait() async {
+        await group.wait()
+    }
+
+    init() {}
+}
+
+// MARK: - Private Interface for Async Usage -
+
+@available(macOS 10.15, *)
+fileprivate actor AsyncGroup {
+    private var taskCount = 0
+    private var continuation: CheckedContinuation<Void, Never>?
+    private var isWaiting = false
+    private var notifyHandlers: [@Sendable () async -> Void] = []
+
+    func enter() {
+        taskCount += 1
+    }
+
+    func leave() {
+        defer {
+            checkCompletion()
+        }
+        guard taskCount > 0 else {
+            assertionFailure("leave() called more times than enter()")
+            return
+        }
+        taskCount -= 1
+    }
+
+    func notify(handler: @escaping @Sendable () async -> Void) {
+        notifyHandlers.append(handler)
+        checkCompletion()
+    }
+
+    func wait() async {
+        if taskCount <= 0 {
+            return
+        }
+
+        isWaiting = true
+
+        await withCheckedContinuation { (continuation: CheckedContinuation<Void, Never>) in
+            self.continuation = continuation
+            checkCompletion()
+        }
+    }
+
+    private func checkCompletion() {
+        if taskCount <= 0 {
+            if isWaiting {
+                continuation?.resume()
+                continuation = nil
+                isWaiting = false
+            }
+
+            if !notifyHandlers.isEmpty {
+                let handlers = notifyHandlers
+                notifyHandlers.removeAll()
+
+                for handler in handlers {
+                    Task {
+                        await handler()
+                    }
+                }
+            }
+        }
+    }
+}
+
+#endif // #if os(WASI) && !canImport(Dispatch)
---
+++ Sources/AsyncKit/DispatchAsyncWasm/DispatchQueue.swift
@@ -0,0 +1,90 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift.org project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift.org project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+#if os(WASI) && !canImport(Dispatch)
+
+/// `DispatchQueue` is a drop-in replacement for the `DispatchQueue` implemented
+/// in Grand Central Dispatch. However, this class uses Swift Concurrency, instead of low-level threading API's.
+///
+/// The primary goal of this implementation is to enable WASM support for Dispatch.
+///
+/// Refer to documentation for the original [DispatchQueue](https://developer.apple.com/documentation/dispatch/dispatchqueue)
+/// for more details,
+@available(macOS 10.15, *)
+class DispatchQueue: @unchecked Sendable {
+    static let main = DispatchQueue(isMain: true)
+
+    private static let _global = DispatchQueue()
+    static func global() -> DispatchQueue {
+        Self._global
+    }
+
+    enum Attributes {
+        case concurrent
+    }
+
+    private let targetQueue: DispatchQueue?
+
+    /// Indicates whether calling context is running from the main DispatchQueue instance, or some other DispatchQueue instance.
+    @TaskLocal static var isMain = false
+
+    /// This is set during the initialization of the DispatchQueue, and controls whether `async` calls run on MainActor or not
+    private let isMain: Bool
+    private let label: String?
+    private let attributes: DispatchQueue.Attributes?
+
+    convenience init(
+        label: String? = nil,
+        attributes: DispatchQueue.Attributes? = nil,
+        target: DispatchQueue? = nil
+    ) {
+        self.init(isMain: false, label: label, attributes: attributes, target: target)
+    }
+
+    private init(
+        isMain: Bool,
+        label: String? = nil,
+        attributes: DispatchQueue.Attributes? = nil,
+        target: DispatchQueue? = nil
+    ) {
+        self.isMain = isMain
+        self.label = label
+        self.attributes = attributes
+        self.targetQueue = target
+    }
+
+    func async(
+        execute work: @escaping @Sendable @convention(block) () -> Void
+    ) {
+        if let targetQueue, targetQueue !== self {
+            // Recursively call this function on the target queue
+            // until we reach a nil queue, or this queue.
+            targetQueue.async(execute: work)
+        } else {
+            if isMain {
+                Task { @MainActor [work] in
+                    DispatchQueue.$isMain.withValue(true) { @MainActor [work] in
+                        work()
+                    }
+                }
+            } else {
+                Task {
+                    work()
+                }
+            }
+        }
+    }
+}
+
+#endif // #if os(WASI) && !canImport(Dispatch)
