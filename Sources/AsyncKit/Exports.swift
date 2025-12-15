b4ce751cfb3fc3477890782db4f5c7b7ed5bb0d1
Scott Marchant <scottm@passivelogic.com>
2025-12-15 15:09:08 -0700

    build: Use canImports to conditionalize exporting of NIOEmbedded and NIOPosix, to avoid breaking changes while allowing wasm to compile.

--- Sources/AsyncKit/Exports.swift
+++ Sources/AsyncKit/Exports.swift
@@ -1,4 +1,10 @@
+#if canImport(NIOEmbedded)
+@_documentation(visibility: internal) @_exported import class NIOEmbedded.EmbeddedEventLoop
+#endif
 @_documentation(visibility: internal) @_exported import protocol NIOCore.EventLoop
 @_documentation(visibility: internal) @_exported import protocol NIOCore.EventLoopGroup
 @_documentation(visibility: internal) @_exported import class NIOCore.EventLoopFuture
 @_documentation(visibility: internal) @_exported import struct NIOCore.EventLoopPromise
+#if canImport(NIOPosix)
+@_documentation(visibility: internal) @_exported import class NIOPosix.MultiThreadedEventLoopGroup
+#endif
